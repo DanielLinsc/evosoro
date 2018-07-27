194f2d109970e35af93c9b38138f87e3123e1858
DanielLinsc <d.linschoten@gmail.com>
2018-07-27 16:26:13 +0200

    Added parallel processing over ssh nodes

---
+++ evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/Utils/Array3D.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/Utils/Array3D.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/Utils/GL_Utils.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/Utils/GL_Utils.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/Utils/MarchCube.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/Utils/MarchCube.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/Utils/Mesh.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/Utils/Mesh.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/Utils/XML_Rip.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/Utils/XML_Rip.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/Utils/tinyxml.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/Utils/tinyxml.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/Utils/tinyxmlerror.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/Utils/tinyxmlerror.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/Utils/tinyxmlparser.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/Utils/tinyxmlparser.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VXS_Bond.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VXS_Bond.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VXS_BondCollision.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VXS_BondCollision.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VXS_BondInternal.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VXS_BondInternal.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VXS_SimGLView.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VXS_SimGLView.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VXS_Voxel.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VXS_Voxel.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_Benchmark.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_Benchmark.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_Bond.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_Bond.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_Environment.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_Environment.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_FEA.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_FEA.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_FRegion.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_FRegion.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_MeshUtil.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_MeshUtil.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_MeshUtil (copy).cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_MeshUtil_(copy).cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_Object.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_Object.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_Sim.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_Sim.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_SimGA.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_SimGA.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_Source.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_Source.cpp.o"
+  "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/VX_Voxel.cpp" "/home/daniel/evosoro/evosoro/_voxcad/Voxelyze/cmake-build-debug/CMakeFiles/Voxelyze.dir/VX_Voxel.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../Utils"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- evosoro/tools/evaluation.py
+++ evosoro/tools/evaluation.py
@@ -15,7 +15,9 @@ from read_write_voxelyze import read_voxlyze_results, write_voxelyze_file
 # sub.call("cp ../_voxcad/voxelyzeMain/voxelyze .", shell=True)
 # sub.call("cp ../_voxcad/qhull .", shell=True)
 
-
+#zelf ingevoegd hieronder
+node = ['node11','node12','node13','node14','node15']
+number_nodes = len(node)
 def evaluate_all(sim, env, pop, print_log, save_vxa_every, run_directory, run_name, max_eval_time=60,
                  time_to_try_again=10, save_lineages=False):
     """Evaluate all individuals of the population in VoxCad.
@@ -75,9 +77,13 @@ def evaluate_all(sim, env, pop, print_log, save_vxa_every, run_directory, run_na
             num_evaluated_this_gen += 1
             pop.total_evaluations += 1
             ids_to_analyze += [ind.id]
-
-            sub.Popen("./voxelyze  -f " + run_directory + "/voxelyzeFiles/" + run_name + "--id_%05i.vxa" % ind.id,
+            #invoegen try nodexxx om te kijken of het werkt, of implementeren dat het in het main script al gevraagd wordt of je met nodes werkt
+            sub.Popen("ssh " + node[ind.id%number_nodes] + " \"cd evosoro/evosoro/afstuderen && ./voxelyze  -f " + run_directory + "/voxelyzeFiles/" + run_name + "--id_%05i.vxa\"" % ind.id,
                       shell=True)
+           # sub.Popen("ssh " + node + "cd evosoro/evosoro/afstuderen && ./voxelyze  -f " + run_directory + "/voxelyzeFiles/" + run_name + "--id_%05i.vxa" % ind.id,
+                    #  shell=True)	
+            #sub.Popen("./voxelyze  -f " + run_directory + "/voxelyzeFiles/" + run_name + "--id_%05i.vxa" % ind.id,
+                      #shell=True)
 
     print_log.message("Launched {0} voxelyze calls, out of {1} individuals".format(num_evaluated_this_gen, len(pop)))
 
